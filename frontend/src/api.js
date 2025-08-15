3b8c69d57d1b73860e74eccd4769bc667c6324ef
Kaire <tranvanquyen5161@gmail.com>
2025-08-15 13:53:33 +0700

    api changes

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "http://localhost:5000",
-});
+const API_URL = "https://my-backend-n3nx.onrender.com"; // your backend URL
 
-export default api;
+export default axios.create({
+  baseURL: API_URL,
+});
